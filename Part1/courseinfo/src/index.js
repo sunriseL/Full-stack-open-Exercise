31acc12f2cf87abe7df3b129c5c9cc3f570d3e33
sunrise <sunrise_l@sjtu.edu.cn>
2020-11-10 15:55:08 +0800

    Part1/courseinfo: finish step 1.5

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
@@ -29,27 +29,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total  parts={parts}/>
+      <Content parts={course.parts}/>
+      <Total  parts={course.parts}/>
     </div>
   )
 }
