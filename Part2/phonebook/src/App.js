daaa52282661e7b93c014bcd3036c86a735e5b71
sunrise <sunrise_l@sjtu.edu.cn>
2020-11-12 14:07:45 +0800

    Part2/phonebook: finish step 2.6

---
+++ Part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import './App.css';
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson} >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return (
+          <div key={person.name} >
+            {person.name}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App;
